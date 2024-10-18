f4a5ac44f21d17e2d620a7bbec9f902287ad6630
carlos manuel pech medina <160980176+ToonCharly@users.noreply.github.com>
2024-10-18 13:56:22 -0600

    Update app.js

--- Dogzline-api/app.js
+++ Dogzline-api/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+require('dotenv').config();
 
 // Middleware para parsear JSON
 app.use(express.json());
